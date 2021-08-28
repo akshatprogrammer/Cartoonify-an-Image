1d4b23ab194c40dfaf4a8dc9c84f9a839120f817
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2021-08-28 11:04:42 +0530

    "Imported Library File"

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2          # for image processing
+import easygui      # to open the File box
+import numpy as np  # to store image
+import imageio      # to read image stored at particular path
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
